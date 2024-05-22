0c2ffeb6939423ab56a659ce685fd22da3dd8df0
Roman Himmes <Roman.Himmes@inform-software.com>
2024-05-22 13:53:01 +0200

    Initial commit

---
+++ compiler_error/main.cpp
@@ -0,0 +1,50 @@
+
+#include <botan/auto_rng.h>
+#include <botan/block_cipher.h>
+#include <botan/base64.h>
+#include <botan/bcrypt.h>
+#include <botan/block_cipher.h>
+#include <botan/cipher_mode.h>
+#include <botan/data_src.h>
+#include <botan/ecdsa.h>
+#include <botan/hash.h>
+#include <botan/hex.h>
+#include <botan/mac.h>
+#include <botan/pubkey.h>
+#include <botan/pkcs8.h>
+#include <botan/system_rng.h>
+#include <botan/x509_key.h>
+
+namespace BotanBackend
+{
+  class PubKeyImpl;
+  class PrvKeyImpl;
+}
+
+class BotanBackend::PubKeyImpl
+{
+public:
+  Botan::ECDSA_PublicKey myPubKey;
+};
+
+class BotanBackend::PrvKeyImpl
+{
+public:
+  Botan::ECDSA_PrivateKey myPrvKey;
+};
+
+
+class CErrorTest
+{
+public:
+  void GetPublicKey() {
+    auto* impl = new BotanBackend::PubKeyImpl{ Botan::ECDSA_PublicKey(myImpl->myPrvKey.algorithm_identifier(), myImpl->myPrvKey.public_key_bits()) };
+  }
+
+  std::shared_ptr< BotanBackend::PrvKeyImpl> myImpl;
+};
+
+int main(int argc, char* argc[])
+{
+
+}
